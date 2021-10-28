d4f0e31f3a8de036ab30feb0282afb041c8db204
Romain DAUNAS <romain.daunas@gmail.com>
2021-10-28 12:51:35 +0200

    Components List

---
+++ kasa_front/src/Components/Nav/Nav.jsx
@@ -0,0 +1,16 @@
+
+import React from "react";
+import { Link } from "react-router-dom";
+
+class Nav extends React.Component {
+
+    render() {
+      return (
+        <div>
+          <img alt=""/>
+          <Link to="/"></Link>
+          <Link to="/apropos"></Link>  
+        </div>
+      );
+    }
+  }
